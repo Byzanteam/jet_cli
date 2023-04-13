22233562d0f3cb598594c6ad0d7cdc8cae20df86
Phil Chen <06fahchen@gmail.com>
2023-04-13 10:16:57 +0800

    chore: bump version to 0.2.3

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule JetCli.MixProject do
   def project do
     [
       app: :jet_cli,
-      version: "0.2.2",
+      version: "0.2.3",
       elixir: "~> 1.14",
       description: "The CLI for Jet Team.",
       source_url: "https://github.com/Byzanteam/jet_cli",
